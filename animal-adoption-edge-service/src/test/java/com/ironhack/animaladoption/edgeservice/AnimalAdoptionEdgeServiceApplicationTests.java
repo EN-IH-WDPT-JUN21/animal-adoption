6eced6dd61d223db77370b3683aa9a1f9ab1de6e
patrykwieteska <patrykwieteskapw@gmail.com>
2021-10-30 09:56:11 +0200

    initial commit

---
+++ animal-adoption-edge-service/src/test/java/com/ironhack/animaladoption/edgeservice/AnimalAdoptionEdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.animaladoption.edgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalAdoptionEdgeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
