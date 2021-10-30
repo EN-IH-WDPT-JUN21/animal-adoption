180fc1440395cee7a2fdfd0c82310aaa3f998b7f
Lee Dawson <lee.dawson1982@hotmail.co.uk>
2021-10-30 10:39:16 +0100

    endpoint updated

--- src/main/java/com/Ironhack/miniadopterprojectadopter/proxy/AnimalProxy.java
+++ src/main/java/com/Ironhack/miniadopterprojectadopter/proxy/AnimalProxy.java
@@ -4,11 +4,18 @@ import com.Ironhack.miniadopterprojectadopter.dto.AnimalDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient("animal-service")
 public interface AnimalProxy {
 
     @GetMapping("api/animals/{id}")
     public AnimalDTO findById(@PathVariable("id") Long id);
+
+
+    @PutMapping("/{id}")
+    public AnimalDTO updateStatus(@PathVariable(name="id") Long id,
+                               @RequestBody AnimalDTO animalDTO);
 
 }
